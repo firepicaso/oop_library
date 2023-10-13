8eb11786dae8307df44cdc341e16bc538abc7a89
Mustakim <m.m.kiron@gmail.com>
2023-10-13 18:49:21 +0600

    Add random id generation

--- Student.rb
+++ Student.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(_id, age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
--- Teacher.rb
+++ Teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
-    super(id, name, age, parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission = true)
+    super(name, age, parent_permission)
     @specialization = specialization
   end
 
