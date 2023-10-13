48edf44f208bb288745b10896d035a8cee312089
Mustakim <m.m.kiron@gmail.com>
2023-10-13 16:58:01 +0600

    Fix file names

--- Person.rb
+++ Person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def initialize(id, name = 'Unknown', age, parent_permission = true)
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    private
+  private
 
-    def of_age?
-        @age >= 18
-    end
+  def of_age?
+    @age >= 18
+  end
 end
--- Teacher.rb
+++ Teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-    def initialize(id, name = 'Unknown', age, parent_permission = true, specialization)
+    def initialize(id, age, name = 'Unknown', parent_permission: true, specialization)
         super(id, name, age, parent_permission)
         @specialization = specialization
     end
@@ -9,4 +9,5 @@ class Teacher < Person
     def can_use_services?
         true
     end
+
 end
