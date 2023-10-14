f55de2e8fb1b39b6d5508e1ca1b100a8c5fc355d
Mustakim <m.m.kiron@gmail.com>
2023-10-14 13:15:51 +0600

    Add rental and book classes

---
+++ rental.rb
@@ -0,0 +1,19 @@
+class Rental
+    attr_accessor :date, :book, :person
+
+    def initialize(date, book, person)
+        @date = date
+        self.book = book
+        self.person = person
+    end
+
+    def book=(book)
+        @book = book
+        book.rentals << self unless book.rentals.include?(self)
+    end
+
+    def person=(person)
+        @person = person
+        person.rentals << self unless person.rentals.include?(self)
+    end
+end
