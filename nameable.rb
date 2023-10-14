0a6060d903e621e95f8de8b684489508c4623daa
Mustakim <m.m.kiron@gmail.com>
2023-10-14 10:30:34 +0600

    Fix linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-        raise NotImplementedError, 'Please implement the correct_name method'
-    end
+  def correct_name
+    raise NotImplementedError, 'Please implement the correct_name method'
+  end
 end
