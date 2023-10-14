0be69cd4b4a70b06c207235bd5438cafddbcef2b
Mustakim <m.m.kiron@gmail.com>
2023-10-14 18:24:51 +0600

    Add bug less obvious

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,6 @@
 require_relative 'person'
 
 class Teacher < Person
-  # attr_accessor :specialization
-
   def initialize(age, name = 'Unknown', parent_permission: true, specialization: 'Unknown')
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
