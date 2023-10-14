433a9def4189eac4306e1777233a87d1a1d57a5e
Mustakim <m.m.kiron@gmail.com>
2023-10-14 18:23:25 +0600

    Add bug

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_accessor :specialization
+  # attr_accessor :specialization
 
   def initialize(age, name = 'Unknown', parent_permission: true, specialization: 'Unknown')
     super(age, name, parent_permission: parent_permission)
